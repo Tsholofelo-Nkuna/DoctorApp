4c7cf0ecda3356045766e6ad70a3b0a491946932
Tsholofelo <tgnkuna768@gmail.com>
2026-06-14 14:26:29 +0200

    Fix mapper logic

--- DoctorManagement.BusinessLogicLayer/Services/Base/BaseMapper.cs
+++ DoctorManagement.BusinessLogicLayer/Services/Base/BaseMapper.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,10 +21,34 @@ namespace DoctorManagement.BusinessLogicLayer.Services.Base
             }
             else
             {
-                return source.Adapt<TDestination>();
+                return Map<TSource, TDestination > (source);
             }
         }
 
-     
+        public static TDestination Map<TSource, TDestination>(TSource source)
+          where TDestination : new()
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            TDestination destination = new TDestination();
+
+            var sourceProperties = typeof(TSource).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var destProperties = typeof(TDestination).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var destProp in destProperties)
+            {
+                var sourceProp = sourceProperties.FirstOrDefault(p => p.Name == destProp.Name
+                                                                     && destProp.CanWrite
+                                                                     && p.PropertyType == destProp.PropertyType);
+                if (sourceProp != null)
+                {
+                    var value = sourceProp.GetValue(source, null);
+                    destProp.SetValue(destination, value, null);
+                }
+            }
+
+            return destination;
+        }
+
     }
 }
