ddcba92fb214da5f0740ea48d6fae4b07bb51402
Tsholofelo <tgnkuna768@gmail.com>
2026-06-23 02:21:33 +0200

    Add MediaHelper

---
+++ DoctorManagement.MAUI/Helpers/MediaHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoctorManagement.MAUI.Helpers
+{
+    public static class MediaHelper
+    {
+        public static void TakePhoto(out string photoFileName, out string photoMimeType, out byte[] photoContents)
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                // Launches the native OS camera UI
+                FileResult photo =  MediaPicker.Default.CapturePhotoAsync().Result;
+
+                if (photo != null)
+                {
+                    // Save or process the local file path
+                    string localFilePath = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+                    using Stream sourceStream =  photo.OpenReadAsync().Result;
+                    var photoBytes = new byte[sourceStream.Length];
+                    sourceStream.Read(photoBytes, 0, photoBytes.Length);
+                    photoContents = photoBytes;
+                    photoFileName = photo.FileName;
+                    photoMimeType = photo.ContentType;
+                    using FileStream localFileStream = File.OpenWrite(localFilePath);
+                    sourceStream.CopyToAsync(localFileStream).Wait();
+
+                }
+                else
+                {
+                    photoFileName = string.Empty;
+                    photoMimeType = string.Empty;
+                    photoContents = [];
+                }
+            }
+            else
+            {
+                photoFileName = string.Empty;
+                photoMimeType = string.Empty;
+                photoContents = [];
+            }
+
+        }
+    }
+}
